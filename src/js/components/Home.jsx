8adc7500d1c209c4323c8a1ec3e7433d1a9c3f47
David Leon <ndleon61@gmail.com>
2025-05-29 01:02:53 +0000

    added the inicial code

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,52 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import './home.css';
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+const [toDos, setToDos] = useState([]);
+const [input, setInput] = useState('');
+ 
+const addToDo = () => {
+	if (input.trim()){
+		const newToDo = {
+			id: Date.now(),
+			text: input
+		}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		setToDos([...toDos, newToDo]);
+		setInput('')
+	} else {
+		alert ("Please add a task")
+	};
+};
+
+const deleteToDo = (idToDelete) => {
+	setToDos(toDos.filter((toDo) => toDo.id !==idToDelete));
+};
+
+  return (
+    <div className="container to-do-container">
+		<h3>To-Do List</h3>
+		<div className="header-container">
+			<input type="text" 
+			placeholder="Add a Task" 
+			value={input} 
+			onChange={(e) => setInput(e.target.value)}
+			className="text-input"
+			/>
+			<button className="btn btn-primary" onClick={addToDo}>Add</button>
 		</div>
-	);
+
+		<ul>
+			{toDos.map((toDo) => (
+				<li key={toDo.id} className="individual-task">
+					<span>{toDo.text}</span>
+					<button className="btn btn-danger" onClick={() => deleteToDo(toDo.id)}>Delete</button>
+				</li>
+			))}
+		</ul>
+    </div>
+  );
 };
 
 export default Home;
